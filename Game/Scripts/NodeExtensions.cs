5e1e4f041287c7095f75028971ddac61d39c46b4
Paul Harvey <paul@pauls-place.me.uk>
2025-01-18 10:33:57 +0000

    Updated disable node mechanism

--- Game/Scripts/NodeExtensions.cs
+++ Game/Scripts/NodeExtensions.cs
@@ -1,36 +1,31 @@
-namespace Godot;
+using Godot;
+using static Godot.Node;
+
+namespace Asteroids;
 
 public static class GodotExtensions
 {
     public static void Enable(this Area2D area2D, bool enable)
     {
-        area2D.SetProcess(enable);
-        area2D.SetPhysicsProcess(enable);
-        area2D.SetProcessInput(enable);
+        ((Node)area2D).Enable(enable);
         area2D.Monitorable = enable;
         area2D.Monitoring = enable;
     }
 
     public static void EnableDeferred(this Area2D area2D, bool enable)
     {
-        area2D.CallDeferred(Area2D.MethodName.SetProcess, enable);
-        area2D.CallDeferred(Area2D.MethodName.SetPhysicsProcess, enable);
-        area2D.CallDeferred(Area2D.MethodName.SetProcessInput, enable);
+        ((Node)area2D).EnableDeferred(enable);
         area2D.SetDeferred(Area2D.PropertyName.Monitorable, enable);
         area2D.SetDeferred(Area2D.PropertyName.Monitoring, enable);
     }
 
     public static void Enable(this Node node, bool enable)
     {
-        node.SetProcess(enable);
-        node.SetPhysicsProcess(enable);
-        node.SetProcessInput(enable);
+        node.ProcessMode = enable ? ProcessModeEnum.Inherit : ProcessModeEnum.Disabled;
     }
 
     public static void EnableDeferred(this Node node, bool enable)
     {
-        node.CallDeferred(Area2D.MethodName.SetProcess, enable);
-        node.CallDeferred(Area2D.MethodName.SetPhysicsProcess, enable);
-        node.CallDeferred(Area2D.MethodName.SetProcessInput, enable);
+        node.SetDeferred(Node.PropertyName.ProcessMode, enable ? (int)ProcessModeEnum.Inherit : (int)ProcessModeEnum.Disabled);
     }
 }
